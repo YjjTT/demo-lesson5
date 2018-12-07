83bf71e5ea1aea6ecdb3c97e2a6b328e2d70dae9
YjjTT <yjjtt@YjjTT-2.local>
2018-12-07 13:28:25 +0800

    first commit

---
+++ demo.sh
@@ -0,0 +1,12 @@
+if [ -d $1 ]; then
+  echo "$1 已经存在了"
+  exit 1
+else 
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>' > index.html
+  echo 'h1{color: red;}' > css/style.css
+  echo 'var string = "Hello World"\nalert(string)' > js/main.js 	
+  exit 0
+fi
